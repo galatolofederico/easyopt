4faecdc451f1f22632ffbba0abad56ac298b46b1
Federico Galatolo <galatolo.federico@gmail.com>
2021-09-15 16:43:32 +0200

    added number of replicas in config

--- easyopt/cmd.py
+++ easyopt/cmd.py
@@ -14,39 +14,27 @@ def die(str):
     print_color(str, "RED")
     sys.exit(1)
 
-def main():
-    parser = argparse.ArgumentParser()
 
-    parser.add_argument("command", metavar="command", type=str, choices=["create", "agent"], help="Command to execute")
-    parser.add_argument("name", type=str, nargs="?", help="Study name ")
-    parser.add_argument("--config", type=str, default="./easyopt.yml")
+def agent(args):
+    pass
 
-    args = parser.parse_args()
-
-    colorama.init()
-
-    if args.command == "create" and args.name is None:
-        args.name = str(uuid.uuid4())
-    if args.command == "agent" and args.name is None:
-        die("You must specify a study name to run the agent: easyopt agent <study_name>")
-
-    if args.command == "create":
-        create(args)
-    elif args.command == "agent":
-        agent(args)
 
 def create(args):
     if not os.path.exists(args.config):
         die(f"Config file {args.config} do not exists. Create a easyopt.yml file or specify a config file using --config")
 
     config = yaml.load(open(args.config, "r"), Loader=yaml.CLoader)
-    
+
     if "storage" not in config:
         die("You have to specify a 'storage'. You can use any SQLAlchemy storage: https://docs.sqlalchemy.org/en/14/core/engines.html#sqlalchemy.create_engine")
 
     if "direction" not in config:
-        print_color("Optimization direction not specified, assuming 'minimize'", "YELLOW")
+        print_color("Optimization direction ('direction') not specified, assuming 'minimize'", "YELLOW")
         config["direction"] = "minimize"
+
+    if "replicas" not in config:
+        print_color("Number of replicas ('replicas') not specified, assuming 1", "YELLOW")
+        config["replicas"] = "1"
 
     sampler = None
     if "sampler" in config:
@@ -74,3 +62,25 @@ def create(args):
     study.set_user_attr("config", config)
 
     print_color(f"Optuna study created with name {args.name}", "GREEN")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("command", metavar="command", type=str, choices=["create", "agent"], help="Command to execute")
+    parser.add_argument("name", type=str, nargs="?", help="Study name ")
+    parser.add_argument("--config", type=str, default="./easyopt.yml")
+
+    args = parser.parse_args()
+
+    colorama.init()
+
+    if args.command == "create" and args.name is None:
+        args.name = str(uuid.uuid4())
+    if args.command == "agent" and args.name is None:
+        die("You must specify a study name to run the agent: easyopt agent <study_name>")
+
+    if args.command == "create":
+        create(args)
+    elif args.command == "agent":
+        agent(args)
