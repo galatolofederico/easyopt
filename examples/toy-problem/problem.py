bf0a8e64df1e58fa2cdd60c38eb491ee3406ffc9
Federico Galatolo <galatolo.federico@gmail.com>
2021-09-15 15:48:27 +0200

    first commit

---
+++ examples/toy-problem/problem.py
@@ -0,0 +1,15 @@
+import argparse
+import easyopt
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--x", type=float, required=True)
+parser.add_argument("--y", type=float, required=True)
+
+args = parser.parse_args()
+
+def objective(x, y):
+    return x**2 + y**2
+
+F = objective(args.x ,args.y)
+easyopt.objective(F)
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import os
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "README.md"), "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easyopt",
+    version="0.0.1",
+    author="Federico A. Galatolo",
+    author_email="federico.galatolo@ing.unipi.it",
+    description="",
+    url="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    entry_points = {
+        "console_scripts": [
+            "easyopt=easyopt.cmd:main",
+        ],
+    },
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta"
+    ],
+)
