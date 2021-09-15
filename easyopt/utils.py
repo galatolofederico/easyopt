1eab86f701b94828d34d6a8bd8cab2aa27f48d16
Federico Galatolo <galatolo.federico@gmail.com>
2021-09-15 19:42:35 +0200

    send and recv utils

---
+++ easyopt/utils.py
@@ -0,0 +1,17 @@
+import json
+
+
+def recv_until_newline(socket):
+    ret = ""
+    char = ""
+    while char != "\n":
+        char = socket.recv(1).decode("utf-8")
+        ret += char
+    return ret
+
+def recv_object(socket):
+    datagram = recv_until_newline(socket).rstrip()
+    return json.loads(datagram)
+
+def send_object(obj, socket):
+    socket.send((json.dumps(obj)+"\n").encode("utf-8"))
